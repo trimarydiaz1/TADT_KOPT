352f2e34412abf973e156972c4c3aa33a8f090ab
TRIMARY A DIAZ-TORRES <trimary.diaz1@upr.edu>
2025-12-08 21:12:14 -0400

    Actualización Final

--- TADT_KOPT/Form1.Designer.cs
+++ TADT_KOPT/Form1.Designer.cs
@@ -36,7 +36,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtDeptCode = new System.Windows.Forms.TextBox();
-            this.txtOfficePhone = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.txtEmail = new System.Windows.Forms.TextBox();
@@ -48,34 +47,33 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.dgvFaculty = new System.Windows.Forms.DataGridView();
             this.label8 = new System.Windows.Forms.Label();
+            this.txtOfficePhone = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFaculty)).BeginInit();
             this.SuspendLayout();
             // 
             // txtFacultyID
             // 
-            this.txtFacultyID.Location = new System.Drawing.Point(101, 81);
+            this.txtFacultyID.Location = new System.Drawing.Point(108, 79);
             this.txtFacultyID.Name = "txtFacultyID";
-            this.txtFacultyID.ReadOnly = true;
             this.txtFacultyID.Size = new System.Drawing.Size(128, 22);
             this.txtFacultyID.TabIndex = 0;
-            this.txtFacultyID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtFacultyID.TextChanged += new System.EventHandler(this.txtFacultyID_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 85);
+            this.label1.Location = new System.Drawing.Point(25, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Faculty ID";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Location = new System.Drawing.Point(19, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 18);
             this.label2.TabIndex = 2;
@@ -83,23 +81,23 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(101, 122);
+            this.txtFirstName.Location = new System.Drawing.Point(108, 120);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(128, 22);
-            this.txtFirstName.TabIndex = 3;
+            this.txtFirstName.TabIndex = 1;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(101, 163);
+            this.txtLastName.Location = new System.Drawing.Point(108, 161);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(128, 22);
-            this.txtLastName.TabIndex = 4;
+            this.txtLastName.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Location = new System.Drawing.Point(19, 163);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 18);
             this.label3.TabIndex = 5;
@@ -109,7 +107,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 285);
+            this.label4.Location = new System.Drawing.Point(19, 283);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 18);
             this.label4.TabIndex = 11;
@@ -117,23 +115,16 @@
             // 
             // txtDeptCode
             // 
-            this.txtDeptCode.Location = new System.Drawing.Point(101, 286);
+            this.txtDeptCode.Location = new System.Drawing.Point(108, 284);
             this.txtDeptCode.Name = "txtDeptCode";
             this.txtDeptCode.Size = new System.Drawing.Size(128, 22);
-            this.txtDeptCode.TabIndex = 10;
-            // 
-            // txtOfficePhone
-            // 
-            this.txtOfficePhone.Location = new System.Drawing.Point(101, 245);
-            this.txtOfficePhone.Name = "txtOfficePhone";
-            this.txtOfficePhone.Size = new System.Drawing.Size(128, 22);
-            this.txtOfficePhone.TabIndex = 9;
+            this.txtDeptCode.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1, 245);
+            this.label5.Location = new System.Drawing.Point(8, 243);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 18);
             this.label5.TabIndex = 8;
@@ -143,7 +134,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(43, 205);
+            this.label6.Location = new System.Drawing.Point(50, 203);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(45, 18);
             this.label6.TabIndex = 7;
@@ -151,16 +142,16 @@
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(101, 204);
+            this.txtEmail.Location = new System.Drawing.Point(108, 202);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(128, 22);
-            this.txtEmail.TabIndex = 6;
+            this.txtEmail.TabIndex = 3;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(299, 52);
+            this.label7.Location = new System.Drawing.Point(314, 50);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(150, 18);
             this.label7.TabIndex = 12;
@@ -168,51 +159,55 @@
             // 
             // txtSearchLastName
             // 
-            this.txtSearchLastName.Location = new System.Drawing.Point(454, 49);
+            this.txtSearchLastName.Location = new System.Drawing.Point(492, 46);
             this.txtSearchLastName.Name = "txtSearchLastName";
-            this.txtSearchLastName.Size = new System.Drawing.Size(128, 22);
-            this.txtSearchLastName.TabIndex = 13;
+            this.txtSearchLastName.Size = new System.Drawing.Size(251, 22);
+            this.txtSearchLastName.TabIndex = 9;
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.Location = new System.Drawing.Point(302, 315);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(93, 39);
-            this.btnSave.TabIndex = 15;
+            this.btnSave.Size = new System.Drawing.Size(128, 39);
+            this.btnSave.TabIndex = 6;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.button2_Click);
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(502, 315);
+            this.btnDelete.Location = new System.Drawing.Point(527, 315);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(93, 39);
-            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Size = new System.Drawing.Size(124, 39);
+            this.btnDelete.TabIndex = 7;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(683, 315);
+            this.btnClear.Location = new System.Drawing.Point(748, 316);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(93, 39);
-            this.btnClear.TabIndex = 18;
+            this.btnClear.Size = new System.Drawing.Size(123, 39);
+            this.btnClear.TabIndex = 8;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnSearch
             // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.HighlightText;
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(683, 32);
+            this.btnSearch.Location = new System.Drawing.Point(749, 37);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(93, 39);
-            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Size = new System.Drawing.Size(111, 39);
+            this.btnSearch.TabIndex = 10;
             this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // dgvFaculty
             // 
@@ -224,8 +219,11 @@
             this.dgvFaculty.RowHeadersWidth = 51;
             this.dgvFaculty.RowTemplate.Height = 24;
             this.dgvFaculty.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvFaculty.Size = new System.Drawing.Size(474, 221);
+            this.dgvFaculty.Size = new System.Drawing.Size(571, 221);
             this.dgvFaculty.TabIndex = 20;
+            this.dgvFaculty.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFaculty_CellClick);
+            this.dgvFaculty.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFaculty_CellContentClick);
+            this.dgvFaculty.DoubleClick += new System.EventHandler(this.dgvFaculty_DoubleClick);
             // 
             // label8
             // 
@@ -236,13 +234,21 @@
             this.label8.Size = new System.Drawing.Size(321, 25);
             this.label8.TabIndex = 21;
             this.label8.Text = "FACULTY DATA ENTRY FORM";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtOfficePhone
+            // 
+            this.txtOfficePhone.Location = new System.Drawing.Point(109, 243);
+            this.txtOfficePhone.Mask = "(000) 000-0000";
+            this.txtOfficePhone.Name = "txtOfficePhone";
+            this.txtOfficePhone.Size = new System.Drawing.Size(127, 22);
+            this.txtOfficePhone.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 366);
+            this.ClientSize = new System.Drawing.Size(893, 367);
+            this.Controls.Add(this.txtOfficePhone);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.dgvFaculty);
             this.Controls.Add(this.btnSearch);
@@ -253,7 +259,6 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtDeptCode);
-            this.Controls.Add(this.txtOfficePhone);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtEmail);
@@ -281,7 +286,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtDeptCode;
-        private System.Windows.Forms.TextBox txtOfficePhone;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtEmail;
@@ -293,6 +297,7 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DataGridView dgvFaculty;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox txtOfficePhone;
     }
 }
 
--- TADT_KOPT/Form1.cs
+++ TADT_KOPT/Form1.cs
@@ -1,40 +1,302 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace TADT_KOPT
 {
     public partial class Form1 : Form
     {
+        int editOk = 0;
+        private readonly string connectionString = Properties.Settings.Default.connString;
+        private int facultyID = 0;
+
         public Form1()
         {
             InitializeComponent();
+            LoadFacultyGrid();
+            ClearForm();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        void IDExists(ref int editOk)
+        {
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+            {
+                // Abre la conexion:
+                connection.Open();
+
+                SqlDataAdapter SqlDa = new SqlDataAdapter("SELECT * FROM Faculty WHERE FacultyID='" + txtFacultyID.Text + "'", connection);  //SqlDa=tabla con ID indicado.
+
+                DataTable dt = new DataTable();//dt= variable de una tabla.
+
+                SqlDa.Fill(dt);//llena la variable dt con la tabla de SqlDa.
+
+                int i = dt.Rows.Count;  //cuenta el número de filas de dt.
+                                        //Si i=0, es un registro nuevo. Si i>0, encontramos un registro existente con ese id.
+                if (i > 0)
+                {
+                    editOk = 1;
+                    MessageBox.Show("FacultyID already exists. The record will be updated.");
+                    dt.Clear();
+                }
+                else
+                {
+                    editOk = 0;
+                  
+                }
+                connection.Close();
+            }
+        }
+        private void ClearForm()
+        {
+            txtFacultyID.Text = "0";
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtEmail.Clear();
+            txtOfficePhone.Clear();
+            txtDeptCode.Clear();
+            txtSearchLastName.Clear();
+
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        
+        private void LoadFacultyGrid(string lastNameFilter = "")
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = sqlCon;
+
+                    if (string.IsNullOrWhiteSpace(lastNameFilter))
+                    {
+                        cmd.CommandText = "FacultyViewAll";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                    }
+                    else
+                    {
+                        cmd.CommandText = "FacultySearchByLastName";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@LastName", lastNameFilter);
+                    }
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dgvFaculty.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // VALIDACIÓN DE CAMPOS VACÍOS
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtOfficePhone.Text) ||
+                string.IsNullOrWhiteSpace(txtDeptCode.Text))
+            {
+                MessageBox.Show("All fields must be filled in before saving.",
+                    "Missing Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // VALIDACIÓN DE EMAIL @hu.edu
+            if (!txtEmail.Text.Trim().EndsWith("@hu.edu"))
+            {
+                MessageBox.Show("Email must end with @hu.edu",
+                    "Invalid Email Format", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                IDExists(ref editOk);
+                using (SqlCommand cmd = new SqlCommand("FacultyAddOrEdit", sqlCon))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //// 🔹 AQUÍ DECIDIMOS SI ES INSERT (0) O UPDATE (1)
+                    //int editOk;
+
+                    //if (string.IsNullOrWhiteSpace(txtFacultyID.Text))
+                    //{
+                    //    // Nuevo registro → INSERT
+                    //    editOk = 0;
+                    //}
+                    //else
+                    //{
+                    //    // Registro existente → UPDATE
+                    //    editOk = 1;
+                    //}
+
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            using (SqlCommand sqlCommand = new SqlCommand("FacultyAddOrEdit", connection))
+                            {
+                                connection.Open();
+
+
+                                IDExists(ref editOk);
+
+                                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                                // Parámetros 
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@editok", SqlDbType.Int));
+                                sqlCommand.Parameters["@editok"].Value = editOk;
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@FacultyID", SqlDbType.VarChar, 25));
+                                sqlCommand.Parameters["@FacultyID"].Value = txtFacultyID.Text.Trim();
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@FirstName", SqlDbType.VarChar, 25));
+                                sqlCommand.Parameters["@FirstName"].Value = txtFirstName.Text.Trim();
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@LastName", SqlDbType.VarChar, 25));
+                                sqlCommand.Parameters["@LastName"].Value = txtLastName.Text.Trim();
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@Email", SqlDbType.VarChar, 100));
+                                sqlCommand.Parameters["@Email"].Value = txtEmail.Text.Trim();
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@OfficePhone", SqlDbType.VarChar, 20));
+                                sqlCommand.Parameters["@OfficePhone"].Value = txtOfficePhone.Text.Trim();
+
+                                sqlCommand.Parameters.Add(new SqlParameter("@DeptCode", SqlDbType.VarChar, 10));
+                                sqlCommand.Parameters["@DeptCode"].Value = txtDeptCode.Text.Trim();
+
+                                sqlCommand.ExecuteNonQuery();
+                            }
+                        }
+
+                        MessageBox.Show("Record saved successfully.",
+                            "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        ClearForm();
+                        LoadFacultyGrid();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Database Operation failed!");
+                    }
+
+                    ClearForm();
+                    LoadFacultyGrid();
+                }
+            }
+        }
+
+        // DELETE
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtFacultyID.Text == "0" || string.IsNullOrWhiteSpace(txtFacultyID.Text))
+            {
+                MessageBox.Show("Select a record to delete first.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show("Are you sure you want to delete this record?",
+                    "Confirm delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                using (SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM faculty WHERE FacultyID = @FacultyID", sqlCon))
+                {
+                    cmd.Parameters.AddWithValue("@FacultyID", txtFacultyID.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Record deleted.", "Info",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            Clear();
+            LoadFacultyGrid();
+        }
+
+        // CLEAR
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        // SEARCH
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadFacultyGrid(txtSearchLastName.Text.Trim());
+
+        }
+        private void dgvFaculty_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            DataGridViewRow row = dgvFaculty.Rows[e.RowIndex];
+
+            txtFacultyID.Text = row.Cells["FacultyID"].Value.ToString();
+            txtFirstName.Text = row.Cells["FirstName"].Value.ToString();
+            txtLastName.Text = row.Cells["LastName"].Value.ToString();
+            txtEmail.Text = row.Cells["Email"].Value.ToString();
+            txtOfficePhone.Text = row.Cells["OfficePhone"].Value.ToString();
+            txtDeptCode.Text = row.Cells["DeptCode"].Value.ToString();
+
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+        }
+
+        private void dgvFaculty_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void txtFacultyID_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        void Clear()
         {
+            txtFacultyID.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtOfficePhone.Text = "";
+            txtDeptCode.Text = "";
 
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+
+            txtFacultyID.Focus();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+        private void dgvFaculty_DoubleClick(object sender, EventArgs e)
         {
-
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
+            if (dgvFaculty.CurrentRow != null)
+            {
+                txtFacultyID.Text = dgvFaculty.CurrentRow.Cells[0].Value.ToString();
+                txtFirstName.Text = dgvFaculty.CurrentRow.Cells[1].Value.ToString();
+                txtLastName.Text = dgvFaculty.CurrentRow.Cells[2].Value.ToString();
+                txtEmail.Text = dgvFaculty.CurrentRow.Cells[3].Value.ToString();
+                txtOfficePhone.Text = dgvFaculty.CurrentRow.Cells[4].Value.ToString();
+                txtDeptCode.Text = dgvFaculty.CurrentRow.Cells[5].Value.ToString();
+            }
         }
     }
 }
+          
